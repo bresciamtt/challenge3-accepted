4505c264ff26beee128cfc8be6a510c1ac59bdba
bresciamtt <bresciamtt@gmail.com>
2021-02-17 00:37:08 +0100

    [#2] Add outputs to local file

--- static-html-on-s3/terraform/main.tf
+++ static-html-on-s3/terraform/main.tf
@@ -9,8 +9,10 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = local.env_vars[terraform.workspace]
+  region = local.env_vars[terraform.workspace]["aws_region"]
 }
+
+data "aws_caller_identity" "current" {}
 
 # S3 bucket
 resource "aws_s3_bucket" "website" {
@@ -20,4 +22,15 @@ resource "aws_s3_bucket" "website" {
   website {
     index_document = "index.html"
   }
+}
+
+# Handle file outputs
+resource "local_file" "output" {
+  content  = <<EOF
+S3_BUCKET_NAME="${aws_s3_bucket.website.bucket}"
+AWS_ACCOUNT_ID="${data.aws_caller_identity.current.account_id}"
+AWS_REGION="${aws_s3_bucket.website.region}"
+EOF
+  filename = "terraform_out"
+  file_permission = "0744"
 }
