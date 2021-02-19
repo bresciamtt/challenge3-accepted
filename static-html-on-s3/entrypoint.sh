028b5625852a83f8a6b9ed148b90b04ae78515c6
bresciamtt <bresciamtt@gmail.com>
2021-02-19 11:02:23 +0100

    [#10][#5] Fix Terraform workspace name and reformat output

--- static-html-on-s3/entrypoint.sh
+++ static-html-on-s3/entrypoint.sh
@@ -8,7 +8,7 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-TF_ENVIRONMENT=${environment:-development}
+TF_ENVIRONMENT=${environment:-dev}
 AWS_ACCESS_KEY_ID=${aws_access_key_id}
 AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 
@@ -31,10 +31,15 @@ fi
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
+echo '### Provisioning the S3 bucket...'
 terraform -chdir=/workspace/terraform init -reconfigure
 terraform -chdir=/workspace/terraform workspace new $TF_ENVIRONMENT
 terraform -chdir=/workspace/terraform workspace select $TF_ENVIRONMENT
 terraform -chdir=/workspace/terraform apply -auto-approve
-
+echo '### The S3 bucket has been provisioned!'
+echo '### Deploying the website...'
 source /workspace/terraform/terraform_out
 aws s3 sync ./out s3://$S3_BUCKET_NAME --delete --acl=public-read
+echo '### The website has been deployed!'
+echo '### Call the following endpoint to access your app: '
+echo "http://$S3_BUCKET_NAME.s3-website.$AWS_REGION.amazonaws.com/"
